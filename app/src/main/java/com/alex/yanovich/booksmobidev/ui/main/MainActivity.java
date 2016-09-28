59e8adf0cfc117d9db3cfaa9d48138b3e4f7b191
Alex Yanov <yanovichalexandr@gmail.com>
2016-09-28 14:00:13 +0300

    Add BooksAdapter

--- app/src/main/java/com/alex/yanovich/booksmobidev/ui/main/MainActivity.java
+++ app/src/main/java/com/alex/yanovich/booksmobidev/ui/main/MainActivity.java
@@ -2,10 +2,24 @@ package com.alex.yanovich.booksmobidev.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.alex.yanovich.booksmobidev.R;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_TRIGGER_SYNC_FLAG =
+            "com.alex.yanovich.booksmobidev.ui.main.MainActivity.EXTRA_TRIGGER_SYNC_FLAG";
+    @Inject
+    MainPresenter mMainPresenter;
+    @Inject BooksAdapter mBooksAdapter;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
