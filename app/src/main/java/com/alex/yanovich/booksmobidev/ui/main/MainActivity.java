cd443a6fea6b5a5ad1d1ff55c98502b7df08834f
Alex Yanov <yanovichalexandr@gmail.com>
2016-09-30 10:54:17 +0300

    Save request in onsave when activity recreates by sys

--- app/src/main/java/com/alex/yanovich/booksmobidev/ui/main/MainActivity.java
+++ app/src/main/java/com/alex/yanovich/booksmobidev/ui/main/MainActivity.java
@@ -2,7 +2,6 @@ package com.alex.yanovich.booksmobidev.ui.main;
 
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.drawable.GradientDrawable;
 import android.support.v4.view.MenuItemCompat;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -27,7 +26,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
 
-public class MainActivity extends BaseActivity implements MainMvpView,SearchView.OnQueryTextListener{
+public class MainActivity extends BaseActivity implements MainMvpView, SearchView.OnQueryTextListener {
 
     private static final String EXTRA_TRIGGER_SYNC_FLAG =
             "com.alex.yanovich.booksmobidev.ui.main.MainActivity.EXTRA_TRIGGER_SYNC_FLAG";
@@ -35,9 +34,11 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
     public static final String EXTRA_INTENT_SERVICE_REQUEST_CODE = "com.alex.yanovich.booksmobidev.ui.main.MainActivity.REQUEST_CODE";
     public static final int EXTRA_INTENT_SERVICE_CODE_FIRST_LOAD = 0;
     public static final int EXTRA_INTENT_SERVICE_CODE_LOAD_MORE = 1;
+    public static final String KEY_BUNDLE_ONSAVE_REQUEST = "com.alex.yanovich.booksmobidev.ui.main.MainActivity.KEY_BUNDLE_ONSAVE_REQUEST";
     @Inject
     MainPresenter mMainPresenter;
-    @Inject BooksAdapter mBooksAdapter;
+    @Inject
+    BooksAdapter mBooksAdapter;
 
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
@@ -46,7 +47,7 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
     Toolbar mToolbar;
 
     SearchView mSearchView;
-    private String mCurrentQuery = "";
+    private String mCurrentRequest = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +55,11 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
         getActivityComponent().inject(this);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        if (savedInstanceState != null) {
+            String request = savedInstanceState.getString(KEY_BUNDLE_ONSAVE_REQUEST);
+            mCurrentRequest = request;
+        }
 
         //Setup Toolbar
         setSupportActionBar(mToolbar);
@@ -63,9 +69,9 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
         mRecyclerView.setAdapter(mBooksAdapter);
         //mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         StaggeredGridLayoutManager sgLayoutManager;
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             sgLayoutManager = new StaggeredGridLayoutManager(3, 1);
-        }else{
+        } else {
             sgLayoutManager = new StaggeredGridLayoutManager(2, 1);
         }
 
@@ -75,7 +81,7 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
             @Override
             public void onLoadMore(int page, int totalItemsCount) {
                 Timber.i("onLoadMore method has been triggered, End of LIST");
-                startServiceRequest(mCurrentQuery, EXTRA_INTENT_SERVICE_CODE_LOAD_MORE);
+                startServiceRequest(mCurrentRequest, EXTRA_INTENT_SERVICE_CODE_LOAD_MORE);
             }
         });
 
@@ -83,6 +89,7 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
         mMainPresenter.loadBooks();
 
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -102,7 +109,7 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_one:
                 showToast("Clicked menu one");
                 startServiceRequest("niel gaiman", EXTRA_INTENT_SERVICE_CODE_LOAD_MORE);
@@ -119,24 +126,31 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
     @Override
     public boolean onQueryTextSubmit(String query) {
         showToast(query);
-        mCurrentQuery = query;
+        mCurrentRequest = query;
         //first we need clear our recyclerView(look showBooks() method for details)
         showBooksEmpty();
         startServiceRequest(query, EXTRA_INTENT_SERVICE_CODE_FIRST_LOAD);
 
         // Hide the keyboard and give focus to the list
-       // InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-       // imm.hideSoftInputFromWindow(mSearchView.getWindowToken(), 0);
-       // mRecyclerView.requestFocus();
+        // InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        // imm.hideSoftInputFromWindow(mSearchView.getWindowToken(), 0);
+        // mRecyclerView.requestFocus();
 
         mSearchView.clearFocus();
         return true;
     }
 
+    /*   Save request when activity recreates */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(KEY_BUNDLE_ONSAVE_REQUEST, mCurrentRequest);
+        super.onSaveInstanceState(outState);
+    }
+
     /*
     This method prepare intent and start service that will download information we want
      */
-    private void startServiceRequest(String request, int requestCode){
+    private void startServiceRequest(String request, int requestCode) {
         if (getIntent().getBooleanExtra(EXTRA_TRIGGER_SYNC_FLAG, true)) {
             Intent intent = SyncService.getStartIntent(this);
             intent.putExtra(EXTRA_INTENT_SERVICE_REQUEST, request);
@@ -155,11 +169,11 @@ public class MainActivity extends BaseActivity implements MainMvpView,SearchView
 
         //we will start from the end of the new list, only we find first item that exist in adapter list we will break
         List<Item> adapterList = mBooksAdapter.getAdapterList();
-        for(int i = allItems.size()-1; i>=0; i--){
+        for (int i = allItems.size() - 1; i >= 0; i--) {
             Item itemToCheck = allItems.get(i);
-            if(!adapterList.contains(itemToCheck)){
+            if (!adapterList.contains(itemToCheck)) {
                 adapterList.add(itemToCheck);
-            }else{
+            } else {
                 break;
             }
         }
